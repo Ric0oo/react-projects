89fd6e2254186882721ec51a27d46b2b6bf40eba
Ric0oo <58725640+Ric0oo@users.noreply.github.com>
2024-04-24 12:12:10 +0200

    reorganize the project

---
+++ src/components/FetchMetod.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "FetchMetod.css";
+
+class FetchMetod extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Fetch Method</h1>
+        <p>This is a paragraph</p>
+      </div>
+    );
+  }
+}
+
+export default FetchMetod;
