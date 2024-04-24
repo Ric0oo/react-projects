56c044488d0fe1cdbe351f4e96ee199a65f32569
Ric0oo <58725640+Ric0oo@users.noreply.github.com>
2024-04-24 12:10:14 +0200

    files update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import DynamicData from "../components/DynamicData";
+// import DynamicData from "./components/DynamicData";
+import FetchMetod from "./components/FetchMetod";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <DynamicData />
+    {/* <DynamicData /> */}
+    <FetchMetod />
   </React.StrictMode>
 );
 
