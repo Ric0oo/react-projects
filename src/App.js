594e57301dd2ae65784ac082598e260e6dc7cb1f
Ric0oo <58725640+Ric0oo@users.noreply.github.com>
2024-04-24 11:52:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { id: 1, title: "Wiadomość numer 1", body: "Zawartość Wiadomości nr 1 ..." },
+  { id: 2, title: "Wiadomość numer 2", body: "Zawartość Wiadomości nr 2 ..." },
+  { id: 3, title: "Wiadomość numer 3", body: "Zawartość Wiadomości nr 3 ..." },
+];
+
+setInterval(() => {
+  const index = data.length + 1;
+  data.push({
+    id: index,
+    title: `Wiadomość numer ${index}`,
+    body: `Zawartość Wiadomości nr ${index}...`,
+  });
+}, 4000);
+
+class App extends Component {
+  state = {
+    comments: [...data],
+  };
+
+  getData = () => {
+    if (this.state.comments.length === data.length) {
+      console.log("aktualizacja");
+      this.setState({
+        comments: [...data],
+      });
+    } else {
+      console.log("brak aktualizacji - ta sama treść");
+    }
+  };
+  componentDidMount() {
+    this.idInterval = setInterval(this.getData, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.idInterval);
+  }
+
+  render() {
+    const comments = this.state.comments.map((comment) => (
+      <div key={comment.id}>
+        <h3>{comment.title}</h3>
+        <p>{comment.body}</p>
+      </div>
+    ));
+    return <div className="App">{comments.reverse()}</div>;
+  }
 }
 
 export default App;
