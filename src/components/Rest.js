d9913d077bedef7d5222d11efd30efffc627faa3
Ric0oo <58725640+Ric0oo@users.noreply.github.com>
2024-04-25 15:25:04 +0200

    data update

--- src/components/Rest.js
+++ src/components/Rest.js
@@ -4,10 +4,34 @@ class Rest extends Component {
   state = {
     users: [],
   };
+
+  componentDidMount() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+
+    xhr.onload = () => {
+      console.log(xhr.status);
+      if (xhr.status === 200) {
+        const users = JSON.parse(xhr.responseText);
+        this.setState({ users });
+      }
+    };
+    xhr.send(null);
+  }
+
   render() {
+    const users = this.state.users.map((user) => (
+      <div key={user.id}>
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <p>{user.address.street}</p>
+        <p>{user.address.city}</p>
+        <p>{user.phone}</p>
+      </div>
+    ));
     return (
       <div>
-        <h1>Rest</h1>
+        <h1>{users}</h1>
       </div>
     );
   }
